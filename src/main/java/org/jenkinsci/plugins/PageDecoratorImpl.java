186058527ce279c756dc0fbedf15cb4f28c1ad35
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-06-22 14:51:42 -0700

    Initial version

---
+++ src/main/java/org/jenkinsci/plugins/PageDecoratorImpl.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class PageDecoratorImpl extends PageDecorator {
+}
